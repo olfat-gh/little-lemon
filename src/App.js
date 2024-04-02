2106b9480ebe4a648e58c6ca030c5c9c1513923a
olfat-gh <ghasemolfat.it@gmail.com>
2024-04-01 20:01:36 -0400

    Added pages and nav links

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
-  return <div className="App">Homepage</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <ChakraProvider>
+          <Header />
+          <Main />
+          <Footer />
+        </ChakraProvider>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
