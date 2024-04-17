266138f8ac5e260c809984c5372f54762e84a947
olfat-gh <ghasemolfat.it@gmail.com>
2024-04-16 20:54:33 -0400

    added formik and validation

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -10,48 +10,96 @@ import {
   Select,
   VStack,
 } from "@chakra-ui/react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import { useState } from "react";
 
-const BookingForm = () => {
-  const [availableTimes, setAvailableTimes] = useState(["17:00", "18:00"]);
+const BookingForm = ({ sdate, times, handleDateChange, submitForm }) => {
+  const [isLoading, setLoading] = useState(false);
+  const formik = useFormik({
+    initialValues: {
+      date: sdate,
+      guests: "",
+      time: "",
+      occasion: "Birthday",
+    },
+    onSubmit: (values) => {
+      setLoading(true);
+      submitForm(values);
+    },
+    validationSchema: Yup.object({
+      date: Yup.string().required("Required"),
+      guests: Yup.number()
+        .min(1, "Must be at least 1 person")
+        .max(10, "Must be at max 10 persons")
+        .required("Required"),
+      time: Yup.string().required("Required"),
+      occasion: Yup.string().required("Required"),
+    }),
+  });
 
   return (
     <Center>
       <VStack p={32} alignItems="flex-start">
         <Heading as="h1">Reserve a Table</Heading>
         <Box p={6} rounded="md" w="100%">
-          <form style={{ display: "grid", "max-width": "200px", gap: "20px" }}>
+          <form
+            style={{ display: "grid", "max-width": "200px", gap: "20px" }}
+            onSubmit={formik.handleSubmit}
+          >
             <VStack spacing={4}>
               <FormControl>
                 <FormLabel htmlFor="res-date">Choose date</FormLabel>
-                <Input id="res-date" name="res-date" type="date" />
+                <Input
+                  id="res-date"
+                  name="res-date"
+                  type="date"
+                  value={sdate}
+                  onChange={(e) => {
+                    formik.setFieldValue("date", e.target.value);
+                    handleDateChange(e);
+                  }}
+                />
               </FormControl>
-              <FormControl>
+              <FormControl
+                isInvalid={formik.touched.time && formik.errors.time}
+              >
                 <FormLabel htmlFor="res-time">Choose time</FormLabel>
-                <Select id="res-time" name="res-time">
-                  {availableTimes.map((item) => (
+                <Select
+                  id="res-time"
+                  name="res-time"
+                  {...formik.getFieldProps("time")}
+                >
+                  <option value="">--- Select ---</option>
+                  {times.map((item) => (
                     <option key={item} value={item}>
                       {item}
                     </option>
                   ))}
                 </Select>
+                <FormErrorMessage>{formik.errors.time}</FormErrorMessage>
               </FormControl>
             </VStack>
-            <FormControl>
+            <FormControl
+              isInvalid={formik.touched.guests && formik.errors.guests}
+            >
               <FormLabel htmlFor="guests">Number of guests</FormLabel>
               <Input
                 id="guests"
                 name="guests"
                 type="number"
                 placeholder="1"
-                min="1"
-                max="10"
+                {...formik.getFieldProps("guests")}
               />
-              <FormErrorMessage></FormErrorMessage>
+              <FormErrorMessage>{formik.errors.guests}</FormErrorMessage>
             </FormControl>
             <FormControl>
               <FormLabel htmlFor="occasion">Occasion</FormLabel>
-              <Select id="occasion" name="occasion">
+              <Select
+                id="occasion"
+                name="occasion"
+                {...formik.getFieldProps("occasion")}
+              >
                 <option>Birthday</option>
                 <option>Anniversary</option>
               </Select>
@@ -60,7 +108,7 @@ const BookingForm = () => {
               type="submit"
               colorScheme="yellow"
               width="full"
-              isLoading={false}
+              isLoading={isLoading}
             >
               Make Your reservation
             </Button>
